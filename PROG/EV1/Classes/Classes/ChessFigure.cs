c828149ee205bceeabd6f6d1dbf7f3da51c41ac4
EvanBotors <145196986+EvanBotors@users.noreply.github.com>
2023-11-03 12:58:06 +0100

    copia 14

--- PROG/EV1/Classes/Classes/ChessFigure.cs
+++ PROG/EV1/Classes/Classes/ChessFigure.cs
@@ -11,9 +11,10 @@
         KNIGHT,
         PAWN,
         KING,
-        TOWER,
+        ROOK,
         BISHOP,
         QUEEN,
+        UNKWOWN,
     }
     class ChessFigure
     {
@@ -21,9 +22,28 @@
         private ColorType _color;
         private FigureType _figureType;
 
+        private ChessFigure(ColorType color, FigureType figure)
+        {
+            _color = color;
+            _figureType = figure;
+        }
+
         public bool IsValid()
         {
-
+            if(_color == ColorType.BLACK || _color == ColorType.WHITE)
+            {
+                switch(_figureType)
+                {
+                    case Classes.FigureType.PAWN:  return true;
+                    case Classes.FigureType.KING:  return true;
+                    case Classes.FigureType.ROOK: return true;
+                    case Classes.FigureType.KNIGHT: return true;
+                    case Classes.FigureType.BISHOP: return true;
+                    case Classes.FigureType.QUEEN: return true;
+                    default: return false;
+                }
+            }
+            return false;
         }
         public int GetX()
         {
@@ -35,28 +55,63 @@
         }
         public ColorType GetColor()
         {
-            return _color;
+            return (IsValid()) ? _color : ColorType.UNKNOWN;
         }
         public FigureType FigureType()
         {
-            return _figureType;
+            return (IsValid()) ? _figureType : Classes.FigureType.UNKWOWN;
         }
         internal void MoveTo(int x, int y)
         {
             _x = x;
             _y = y;
-
+            return;
         }
-        public ? Promove()
+        public void Promove(ChessFigure figure, FigureType typePromoved )
         {
-
+            if(figure.FigureType()!=Classes.FigureType.PAWN)
+            {
+                return;
+            }
+            CreateFigure(figure.GetColor(), typePromoved);
+            MoveTo(_x, _y);
+            return;
         }
-        public Rectangle GetRectangle()
+        /*public Rectangle GetRectangle()
         {
-
+            
+        }*/
+        public static ChessFigure? CreateFigure(ColorType color, FigureType figure)
+        {
+            if(color != ColorType.WHITE || color!= ColorType.BLACK)
+            {
+                return null;
+            }
+            if (figure != Classes.FigureType.QUEEN)
+                return null;
+            if (figure != Classes.FigureType.KNIGHT)
+                return null;
+            if (figure != Classes.FigureType.KING)
+                return null;
+            if (figure != Classes.FigureType.BISHOP)
+                return null;
+            if (figure != Classes.FigureType.PAWN)
+                return null;
+            if (figure != Classes.FigureType.ROOK)
+                return null;
+            return new ChessFigure(color, figure);
         }
 
 
-
+    }
+    class Rectangle
+    {
+        private int _x;
+        private int _y;
+        Rectangle(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
     }
 }
--- PROG/EV1/Classes/Classes/ChessUtils.cs
+++ PROG/EV1/Classes/Classes/ChessUtils.cs
@@ -2,15 +2,67 @@
 {
     class ChessUtils
     {
-        public static CanKnightMoveTo(int x, int y, ChessFigure.FigureType chessType, int targetX, int targetY)
+        private int _movCount = 0;
+        public static bool CanKnightMoveTo(int x, int y, FigureType chessType, int targetX, int targetY)
+        {
+            if(chessType== FigureType.KNIGHT)
+            {
+                if((targetX == x+30 || targetX== x-30) && (targetY == y+10 || targetY == y + 10))
+                    return true;
+                else if((targetX == x + 10 || targetX == x - 10) && (targetY == y + 30 || targetY == y + 30))
+                    return true;
+                return false;
+            }
+            return false;
+        }
+        /*public static bool CanPawnMoveTo(int x, int y, FigureType chessType, int targetX, int targetY)
+        {
+            if (chessType == FigureType.PAWN)
+            {
+
+            }
+        }
+        public static bool CanKingMoveTo(int x, int y, FigureType chessType, int targetX, int targetY)
+        {
+            if (chessType == FigureType.KING)
+            {
+
+            }
+        }
+        public static bool CanQueenMoveTo(int x, int y, FigureType chessType, int targetX, int targetY)
+        {
+            if (chessType == FigureType.QUEEN)
+            {
+
+            }
+        }
+        public static bool CanRookMoveTo(int x, int y, FigureType chessType, int targetX, int targetY)
+        {
+            if (chessType == FigureType.ROOK)
+            {
+
+            }
+        }
+        public static bool CanBishopMoveTo(int x, int y, FigureType chessType, int targetX, int targetY)
+        {
+            if (chessType == FigureType.BISHOP)
+            {
+
+            }
+        }*/
 
         public int GetMovementCount()
         {
-            return;
+            return _movCount;
         }
         public bool HasBeenMoved()
         {
-            return GetMovementCount() > 0 ? true : false;0
+            return IsMoving() ? true : false;
         }
+        public bool IsMoving()
+        {
+            return GetMovementCount() > 0;
+        }
+
     }     
 }
