3aa3beb85d7ceee1b96b83d18cee555b42afe167
EvanBotors <145196986+EvanBotors@users.noreply.github.com>
2023-11-20 20:43:46 +0100

    copia 33

--- PROG/EV1/BigNumbers/BigNumbers/Program.cs
+++ PROG/EV1/BigNumbers/BigNumbers/Program.cs
@@ -4,8 +4,8 @@
     {
         static void Main(string[] args)
         {
-            BigNumber n1 = new BigNumber(-7-1);
-            //BigNumber n2 = new BigNumber("");
+            BigNumber n1 = new BigNumber("89");
+            //BigNumber n2 = new BigNumber("111");
             //BigNumber n3 = BigNumber.Add(n1, n2);
             string result = n1.ConvertToString();
             Console.WriteLine(result);
