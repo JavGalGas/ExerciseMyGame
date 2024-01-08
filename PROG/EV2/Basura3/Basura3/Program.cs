148306bfa21c7dcd9b843e34081e8af105f936ee
EvanBotors <145196986+EvanBotors@users.noreply.github.com>
2024-01-08 16:36:56 +0100

    copia 54

---
+++ PROG/EV2/Basura3/Basura3/Program.cs
@@ -0,0 +1,14 @@
+﻿using DAMLib;
+
+namespace Basura3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack s = new Stack();
+            
+            
+        }
+    }
+}
---
+++ PROG/EV2/Basura3/DAMLib/Stack.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.CompilerServices;
+using System.Xml.Linq;
+
+namespace DAMLib
+{
+    public class Stack<T>
+    {
+        private T [] _stack = new T[0];
+        public void Push(T element)
+        {
+            T[] aux = new T[_stack.Length+1];
+            for(int i = 0; i < _stack.Length; i++)
+            {
+                aux[i] = _stack[i];
+            }
+            aux[GetCount()-1] = element;
+            _stack = aux;
+        }
+        public T Pop(T element)
+        {
+            //for()
+            //return _stack[_stack.Length - 1];
+        }
+        public T GetTop()
+        {
+            return _stack[_stack.Length - 1];
+        }
+        //Pop y GetTop pueden dar problemas con el null, hay que buscar sobre el default(T);
+        public bool IsEmpty()
+        {
+            return false;
+        }
+        public int GetCount()
+        {
+            return _stack.Length;
+        }
+        //Libreria
+        // DAMLibTest
+        //      DAMLibTest
+        //      DAMLib (biblioteca de clases)
+    }
+}
