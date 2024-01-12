f4c4ae7c4ed189de2b29dab24f1b0b4040ef521c
EvanBotors <145196986+EvanBotors@users.noreply.github.com>
2024-01-12 18:40:06 +0100

    copia 69

--- PROG/EV2/Basura3/DAMLib/SetWithHash.cs
+++ PROG/EV2/Basura3/DAMLib/SetWithHash.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,20 +6,27 @@ using System.Threading.Tasks;
 
 namespace DAMLib
 {
-
-    public class Set<T>
+    public class SetWithHash<T>
     {
         private T[] _set = new T[0];
-        private int _count = 0;    
+        private int[] _hash = new int[0];
+        private int _count = 0;
+
+
 
         public override bool Equals(object? obj)
         {
             if (this == obj)
                 return true;
-            if (obj is not Set<T>)
+            if (obj is not SetWithHash<T>)
                 return false;
-            Set<T> s = (Set<T>)obj;
+            SetWithHash<T> s = (SetWithHash<T>)obj;
             return s._set == _set && s._count == _count;
+        }
+
+        public override int GetHashCode()
+        {
+            return _hash.GetHashCode() * _hash.GetHashCode() - 66 * (_hash.GetHashCode()/77) + Count;
         }
         public int Count
         {
@@ -29,7 +35,7 @@ namespace DAMLib
 
         public int IndexOf(T element)
         {
-            for(int i = 0; i < _set.Length; i++)
+            for (int i = 0; i < _set.Length; i++)
             {
                 if (_set[i].Equals(element))
                 {
@@ -43,12 +49,13 @@ namespace DAMLib
         {
             _count = 0;
         }
-        public void Add(T element)
+        public void Add(T element) //implementar hash
         {
 
             if (Count < _set.Length)
             {
                 _set[_count++] = element;
+                _hash[Count] = 1;
             }
             else
             {
@@ -61,13 +68,13 @@ namespace DAMLib
                 _set = NewSet;
             }
         }
-        public void Remove(T element)
+        public void Remove(T element) //implementar hash
         {
             int aux = IndexOf(element);
             if (aux == -1)
                 return;
             T[] NewSet = new T[Count - 1];
-            
+
             //for (int i = 0; i < Count - 1; i++)
             //{
             //    if (i == aux)
@@ -75,15 +82,15 @@ namespace DAMLib
             //    NewSet[i] = _set[i];
             //}
 
-            for(int i = 0; i < aux; i++)
-                NewSet[i]=_set[i];
-            for (int i = aux+1; i < Count - 2; i++)
-                NewSet[i-1] = _set[i];
+            for (int i = 0; i < aux; i++)
+                NewSet[i] = _set[i];
+            for (int i = aux + 1; i < Count - 2; i++)
+                NewSet[i - 1] = _set[i];
 
             _set = NewSet;
         }
 
-        public bool Cointains(T element)
+        public bool Cointains(T element) //implementar hash
         {
             //if (IndexOf(element) == -1)
             //    return false;
@@ -97,6 +104,5 @@ namespace DAMLib
             return false;
         }
 
-        
     }
 }
