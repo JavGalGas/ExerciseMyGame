eb50297bcc50ff299013899ca1e9f6ce7e22af8d
JavGalGas <145196986+JavGalGas@users.noreply.github.com>
2024-03-12 20:13:51 +0100

    copia 120

---
+++ PROG/EV3/DominoJGG/DominoJGG/Game.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominoJGG
+{
+    public class Game
+    {
+        private List<Participant> _participants;
+        private DominoDeck _amountOfDominoes;
+        private Participant? _winner;
+
+        public Game(List<Participant> participants, DominoDeck amountOfDominoes) 
+        { 
+            _participants = participants;
+            _amountOfDominoes = amountOfDominoes;
+        }
+        public void StartGame()//
+        {
+
+        }
+        public void SimulateGame()
+        {
+            _amountOfDominoes.Shuffle();
+            foreach (Participant participant in _participants)
+            {
+                _amountOfDominoes.ExtractPiece();
+            }
+        }
+    }
+}
